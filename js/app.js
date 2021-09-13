f30df2a96c18b16b0101d2676de929e8a88c7a82
anamikasharma15 <anamikaanuctg@gmail.com>
2021-09-14 02:55:47 +0600

    final submit

--- js/app.js
+++ js/app.js
@@ -9,22 +9,35 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
+  //by destructuring product object
+     const { rate, count } = product.rating;
+
+    // products details function
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+     div.innerHTML = `<div class="single-product">
+       <div>
+     <img class="product-image" src=${image}></img>
+       </div>
+       <h3>${product.title}</h3>
+       <p>Category: ${product.category}</p>
+       <h2>Price:$ ${product.price}</h2>
+       <h5>Total-Rating :<span><i class="fas fa-user-friends text-success "></i> ${count}</span>  </h5>
+      <h6>Average-rating:<span><i class="fas fa-star text-danger "></i></span>
+                      <span><i class="fas fa-star text-danger "></i></span>
+                      <span><i class="fas fa-star text-danger "></i></span>
+                      <span><i class="fas fa-star text-danger "></i></span>
+                      <span><i class="fas fa-star text-white"></i></span> ${rate} </h6>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
+
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+// total calculation function
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -32,11 +45,12 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -45,12 +59,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText =  Math.round(value);
 };
 
 // update delivery charge and total Tax
@@ -70,11 +84,22 @@ const updateTaxAndCharge = () => {
   }
 };
 
+
+
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+
+  console.log(
+      getInputValue("price"),
+      getInputValue("delivery-charge"),
+      getInputValue("total-tax")
+  );
+
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge") +
+      getInputValue("total-tax");
+
+  console.log(grandTotal)
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
+
 };
 loadProducts();
